70526b1c94028f2ffac3cf4217accae7acecdd53
Shravya M <92004685+shravyam12@users.noreply.github.com>
2023-10-15 11:28:55 +0530

    Update C.c
    I have made  a small change like number of times "hello wold" to print. Thank you.

--- Hello World In All Language/C.c
+++ Hello World In All Language/C.c
@@ -3,7 +3,12 @@
 
 int main()
 {
+    int n;
+    printf("Enter number of times the term should be printed\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    { 
     printf("Hello World <3");
-
+    }
     return EXIT_SUCCESS;
 }
